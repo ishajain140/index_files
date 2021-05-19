6f32617c3b31bbe679f9e0543cdd950b7f449df4
ishajain140 <64470818+ishajain140@users.noreply.github.com>
2021-05-19 15:02:35 +0530

    updated code

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1>Welcome to PHP testpage</h1>
- <?php echo "https://ishajain140.medium.com/"; ?>
 
+<h1> Welcome to PHP test page</h1>
+<a href = "https://ishajain140.medium.com/"Target="_blank">medium profile link</a>
+<pre>
+<?php
+#print ("https://ishajain140.github.com/ ");
+?>
+</pre>
 </body>
-</html> 
+</html>
